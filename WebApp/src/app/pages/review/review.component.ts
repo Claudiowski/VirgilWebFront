2811102cc043d79066de5c2fe0c380f290b56a0a
auguste <auguste@debian>
2017-10-14 14:49:22 +0200

    Added sorting system for review-component

--- WebApp/src/app/pages/review/review.component.ts
+++ WebApp/src/app/pages/review/review.component.ts
@@ -9,16 +9,122 @@ import { ReviewService } from './review.service'
 })
 export class ReviewComponent implements OnInit {
 
-    private favorites : Object[]
+    categories : Object[] = []
+    themes : Object[] = []
+
+    navbarClass : string = "closedNav" 
+
+    selectedThemes : Object[] = []
+    selectedCategories : Object[] = []
+
+    private favorites : Object[] = []
     
     constructor(private _reviewService : ReviewService) { }
 
     ngOnInit() { 
         this.fetchFavorites()
+        this.fetchThemes()        
     }
 
     private fetchFavorites() {
         this._reviewService.fetchFavorites()
                 .then(data => this.favorites = data )
+    }
+
+    openNav() {    
+        this.navbarClass = "openNav"
+    }
+
+    private fetchThemes() {
+        this._reviewService.fetchThemesByReader()
+                .then(data => this.themes = data )
+    }
+
+    private fetchCategoriesByTheme(theme : Object, checkbox_state : boolean) {
+        if (checkbox_state == true) {
+            this._reviewService.fetchCategoriesByTheme(theme['id'])
+                .then(data => this.categories = this.categories.concat(data) ) 
+        } else {
+            this.removeCategoriesByTheme(theme)
+        }
+    }
+
+    private removeCategoriesByTheme(theme) {
+        let c = this.categories
+        let index = 0        
+        for (let i = 0; i < c.length; i++) {
+            if (c[i]['id_theme'] == theme['id']) {
+                index = c.indexOf(c[i])
+                c.splice(index, 1)
+            }
+        }
+    }
+
+    private removeSelectedCategoriesByTheme(theme) {
+        let sc = this.selectedCategories
+        let index = 0   
+        for (let i = 0; i < sc.length; i++) {
+            if (sc[i]['id_theme'] == theme['id']) {
+                index = sc.indexOf(sc[i])
+                sc.splice(index, 1)
+            }
+        }
+    }
+
+    private addThemeToChosenList(theme, box_checked)  {
+        let index = 0
+        if (box_checked) {
+            this.selectedThemes.push(theme)
+        } else {
+            let index = this.selectedThemes.indexOf(theme)
+            this.selectedThemes.splice(index, 1)
+            this.removeCategoriesByTheme(theme)
+            this.removeSelectedCategoriesByTheme(theme)           
+        }
+    }
+
+    private addCategoryToChosenList(category, box_checked) {
+        if (box_checked) {
+            this.selectedCategories.push(category)
+        } else {
+            let index = this.selectedCategories.indexOf(category)
+            this.selectedCategories.splice(index, 1)
+        }
+    }
+
+    closeNav() {
+        this.navbarClass = "closedNav"
+        this.favorites = []
+        if (this.selectedCategories.length == 0 && this.selectedThemes.length == 0) {
+            this.fetchFavorites()
+        } else {
+            let themes = this.getThemesToFetch()
+            this._reviewService.fetchChosenFavorites(themes, this.selectedCategories)
+                .subscribe(data => {
+                    this._reviewService.fetchChosenFavorites(themes, this.selectedCategories)
+                        .subscribe(data => {
+                            this.favorites= this.favorites.concat(data)
+                    })
+                }
+            )
+        }
+    }
+
+    private getThemesToFetch() {
+        const c = this.selectedCategories
+        const t = this.selectedThemes
+        let themes = []        
+        if (c.length == 0) {
+            return t
+        }
+        for (let i = 0; i < t.length; i++) {
+            for (let j = 0; j < c.length; j++) {
+                if (t[i]['id'] == c[j]['id_theme']) 
+                    break
+                if (j+1 == c.length)
+                    themes.push(t[i])
+            }
+        }
+        return themes
     }
 }
--- WebApp/src/app/pages/review/review.service.ts
+++ WebApp/src/app/pages/review/review.service.ts
@@ -2,24 +2,110 @@ import { Component, Injectable } from '@angular/core'
 import { Http, URLSearchParams, RequestOptions } from '@angular/http'
 
 import * as jwt_decode from 'jwt-decode';
-
+import { Observable } from 'rxjs/Observable'
 
 @Injectable()
 export class ReviewService {
 
   constructor(private http : Http) { }
   
-  public fetchFavorites() {
-    let token = sessionStorage.getItem('token')
-    let id_reader = jwt_decode(token)['id']
-    let headers = new Headers()
-    let params = new URLSearchParams()
-    let url = 'http://localhost:8080/favorite-by-reader'
-    headers.append('Authorization', token)
-    params.append('id_reader', id_reader)
-    return this.http.post(url, params, new RequestOptions(headers))
-                    .toPromise()
-                    .then(response => { 
-                        return response.json() })
+    public fetchFavorites() {
+      let token = sessionStorage.getItem('token')
+      let id_reader = jwt_decode(token)['id']
+      let headers = new Headers()
+      let params = new URLSearchParams()
+      let url = 'http://localhost:8080/favorite-by-reader'
+      headers.append('Authorization', token)
+      params.append('id_reader', id_reader)
+      return this.http.post(url, params, new RequestOptions(headers))
+                      .toPromise()
+                      .then(response => { 
+                          return response.json() })
+    }
+
+    public fetchFavoritesByThemes(id_themes) {
+      let token = sessionStorage.getItem('token')
+      let id_reader = jwt_decode(token)['id']
+      let headers = new Headers()
+      let params = {}
+      let url = 'http://localhost:8080/favorite-by-theme-list'
+      params = { 'id_themes': id_themes }      
+      headers.append('Authorization', token)
+      return this.http.post(url, params, new RequestOptions(headers))
+                      .toPromise()
+                      .then(response => { 
+                          return response.json() })
+    }
+
+    public fetchFavoritesByCategories(id_categories) {
+      let token = sessionStorage.getItem('token')
+      let id_reader = jwt_decode(token)['id']
+      let headers = new Headers()
+      let params = {}
+      let url = 'http://localhost:8080/favorite-by-category-list'
+      params = { 'id_categories': id_categories }      
+      headers.append('Authorization', token)
+      return this.http.post(url, params, new RequestOptions(headers))
+                      .toPromise()
+                      .then(response => { 
+                          return response.json() })
+    }
+
+    public fetchThemesByReader() {
+      let token = sessionStorage.getItem('token')
+      let id_reader = jwt_decode(token)['id']
+      let headers = new Headers()
+      let params = new URLSearchParams()
+      let url = 'http://localhost:8080/theme-by-reader'
+      headers.append('Authorization', token)
+      params.append('id_reader', id_reader)
+      return this.http.post(url, params, new RequestOptions(headers))
+                      .toPromise()
+                      .then(response => { 
+                          return response.json() })
+    }
+
+    public fetchCategoriesByTheme(id_theme : string) {
+      let token = sessionStorage.getItem('token')
+      let headers = new Headers()
+      let params = new URLSearchParams()
+      let url = 'http://localhost:8080/category-by-theme-id'
+      headers.append('Authorization', token)
+      params.append('id_theme', id_theme)
+      return this.http.post(url, params, new RequestOptions(headers))
+                      .toPromise()
+                      .then(response => { 
+                          return response.json() })
+  }
+
+  public fetchChosenFavorites(theme_list, cat_list) {
+    let id_themes = []
+    let id_categories = []
+
+    for (let i = 0; i < theme_list.length; i++) 
+        id_themes.push(theme_list[i]['id'])
+
+    for (let i = 0; i < cat_list.length; i++)
+        id_categories.push(cat_list[i]['id'])
+
+    return new Observable(observer => {
+        if (id_categories.length > 0 && id_themes.length > 0) {
+            this.fetchFavoritesByCategories(id_categories)
+                .then(data => {
+                    observer.next(data)
+                    this.fetchFavoritesByThemes(id_themes)
+                        .then(data => {
+                            observer.next(data)
+                            observer.complete()
+                        })   
+            })
+        } else if (id_categories.length > 0 && id_themes.length == 0) {
+            this.fetchFavoritesByCategories(id_categories)
+                .then(data => { observer.next(data); observer.complete() } )
+        } else if (id_themes.length > 0 && id_categories.length == 0) {
+            this.fetchFavoritesByThemes(id_themes)
+                .then(data => { observer.next(data); observer.complete() } )
+        }
+    })
   }
 }
